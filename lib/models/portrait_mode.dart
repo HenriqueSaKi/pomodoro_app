0086fbd3b9fe8aaf93b668117c6f8eb4a2602104
Henrique-Kisaki <satoshikisaki@hotmail.com>
2022-10-16 09:49:32 -0300

    Atualização na lógica do alarme

--- lib/models/portrait_mode.dart
+++ lib/models/portrait_mode.dart
@@ -355,9 +355,14 @@ class _PortraitModeState extends State<PortraitMode> {
                             widget.seconds = 60;
                             widget.minutes--;
                           }
-                          if ((widget.minutes == 0 && widget.seconds == 0) ||
-                              widget.startIsPressed == false) {
+                          if (widget.startIsPressed == false) {
                             timer.cancel();
+                            widget.startIsPressed = false;
+                            setState(() {});
+                          } else if (widget.minutes == 0 &&
+                              widget.seconds == 0) {
+                            timer.cancel();
+                            widget.startIsPressed = false;
                             setState(() {});
                             playDigitalAlarm();
                           } else {
